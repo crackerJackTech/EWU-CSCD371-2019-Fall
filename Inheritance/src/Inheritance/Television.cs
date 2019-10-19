3353ed1cac821f62f6542f89648e799d0c8ecba8
Devin <dkramer2@eagles.ewu.edu>
2019-10-19 11:27:54 -0700

    Added the Television class, included the Size and Manufacturer properties, and included the PrintInfo method

---
+++ Inheritance/src/Inheritance/Television.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Television : Item
+    {
+        public int Size { get; set; }
+        public string Manufacturer { get; set; }
+
+        public override string PrintInfo()
+        {
+            return $"{Size} - {Manufacturer}";
+        }
+    }
+}
