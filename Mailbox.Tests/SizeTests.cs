65c99edbd41f0239d292c7e148c8c34889e97129
Devin Kramer <dkramer2@eagles.ewu.edu>
2019-10-31 17:46:46 -0700

    Updated Size class with Enums, and created SizeTests class

---
+++ Mailbox.Tests/SizeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mailbox.Tests
+{
+    [TestClass]
+    public class SizeTests
+    {
+        [TestMethod]
+        public void EnumBitwiseOperation_ReturnsCorrectValue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("0", Sizes.None);
+            Assert.AreEqual("1", Sizes.Small);
+            Assert.AreEqual("2", Sizes.Medium);
+            Assert.AreEqual("4", Sizes.Large);
+
+        }
+    }
+}
--- Mailbox/Size.cs
+++ Mailbox/Size.cs
@@ -1,4 +1,13 @@
-﻿namespace Mailbox
-{
+﻿using System;
 
+namespace Mailbox 
+{
+    [Flags]
+    public enum Sizes
+    {
+        None = 0b_0000,
+        Small = 0b_0001,
+        Medium = 0b_0010,
+        Large = 0b_0100
+    };
 }
