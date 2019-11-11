9364112b4c5d4988ee44a8eb2b73193312459bd9
Devin <dkramer2@eagles.ewu.edu>
2019-11-10 23:37:21 -0800

    Implements ICollections and required methods. Also created Unit tests for the Arrays class.

---
+++ Arrays.Tests/ArraysTests.cs
@@ -0,0 +1,67 @@
+﻿using Assignment6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Arrays.Tests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(100)]
+        public void Constructor_SizeHasCorrectValue_ArraysAreCreated(int size)
+        {
+            //Arrange
+            var test1 = new Array<int>(size);
+            var test2 = new Array<string>(size);
+            var test3 = new Array<long>(size);
+            //Act
+
+            //Assert
+            Assert.AreEqual(test1.Capacity, size);
+            Assert.AreEqual(test2.Capacity, size);
+            Assert.AreEqual(test3.Capacity, size);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_InitializedArrayWithMoreDataThanSize_ThrowsException()
+        {
+            //Arrange
+            var sizeOfArray = 0;
+            var testArray = new Array<string>(sizeOfArray)
+            {
+                "testString1"
+            };
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void Constructor_SupportsCollectionInitialization_Success()
+        {
+            //Arrange
+            var sizeOfArray = 1;
+
+            //Act
+            var testArray = new Array<string>(sizeOfArray)
+            {
+                "testString1",
+            };
+
+            //Assert
+            Assert.AreEqual(1, testArray.Count);
+        }
+
+
+
+
+
+
+    }
+}
--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class Array<T> : ICollection<T>
     {
+        private List<T> _List;
+        public int Capacity { get; }
+        private int numberOfItemsInArray { get; set; }
+
+        public int Count => numberOfItemsInArray;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
         
+        public Array(int size)
+        {
+            if(size < 0)
+            {
+                throw new ArgumentNullException(nameof(size));
+            }
+            _List = new List<T>(size);
+            Capacity = size;
+        }
+
+        public void Add(T item)
+        {
+            if(Count == Capacity)
+            {
+                throw new ArgumentOutOfRangeException(nameof(_List.Count), "Can't exceed the size of the Array");
+            }
+            if(item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            _List.Add(item);
+            numberOfItemsInArray++;
+            
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
