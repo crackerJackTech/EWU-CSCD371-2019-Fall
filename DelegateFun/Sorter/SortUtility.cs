e0c5dd8d1b72a58aedefe8b53a34ef555d376f07
Devin <dkramer2@eagles.ewu.edu>
2019-11-18 10:57:19 -0800

    Used Insertion Sort as the sorting algorithm. Defined the delegate in the SortUtility class. Created required unit tests.

--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,37 @@
 
 namespace Sorter
 {
-    public class SortUtility
+    public static class SortUtility
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+        public delegate bool SortingTypeFuction(int i, int j);
+
+        public static void InsertionSort(int[] items, SortingTypeFuction compare)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            if(compare is null)
+            {
+                throw new ArgumentNullException(nameof(compare));
+            }
+
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (compare(items[j - 1], items[j]))
+                    {
+                        int tempPlaceHolder = items[j - 1];
+                        items[j - 1] = items[j];
+                        items[j] = tempPlaceHolder;
+                    }
+                }
+            }
+
+        }
     }
 }
